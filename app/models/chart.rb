b3507c6c530a2ba8f69e1d97712cb47b026205e5
Nicholas Humfrey <njh@aelius.com>
2016-07-18 15:40:26 +0100

    Sort charts by date by default

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,5 +1,6 @@
 class Chart < ActiveRecord::Base
   has_many :entries, class_name: "ChartEntry", foreign_key: "chart_id"
+  default_scope { order(date: :asc) }
 
   def tupac_page
     "https://production.live.bbc.co.uk/music/charts/radio1/singles/#{date}/publish"
