780272b7f22f11d4350eea37ffbf76e25e14eabd
Nicholas Humfrey <njh@aelius.com>
2016-07-18 21:01:54 +0100

    Added links to I Like Music

--- app/models/track.rb
+++ app/models/track.rb
@@ -5,6 +5,15 @@ class Track < ActiveRecord::Base
     "https://production.live.bbc.co.uk/music/records/#{record_id}"
   end
   
+  def ilm_info_page
+    "http://desktopjukebox.broadchart.com/bbc/members/asset_info/#{ilm_id.sub('-','/')}"
+  end
+  
+  def ilm_player_page
+    (album_id,track_no) = ilm_id.split('-')
+    "http://desktopjukebox.broadchart.com/bbc/members/player.cgi?album_id=#{album_id}&track_no=#{track_no}"
+  end
+  
   def image_url(size='64x64')
     attributes['image_url'].sub(/\d+x\d+/, size)
   end
